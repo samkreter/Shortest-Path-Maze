f65c4c7f75aa74a1bb6bb01b0e1b1c4018313aa4
Sam Kreter <sakfy6@mail.missouri.edu>
2014-11-15 22:36:45 -0600

    modulizing into classes

--- fileReader.py
+++ fileReader.py
@@ -1,26 +1,39 @@
 
 #makes the map matrix from the file
-def makeMatrix():
+class qObj:
+  def _init_(self,coords,steps):
+    self.coord1 = coord1
+    self.coord2 = coord2
+    self.steps = steps
 
-  matrix = []
+
+class Matrix:
+
+  #takes in a filepath and creates the self.matrix from the text file
+  def _init_(self,filePath):
+    matrix = []
+    for line in open(filePath):
+      temp = []
+      for c in line:
+        temp.append(c);
+      matrix.append(temp);
+    self.matrix = matrix
 
 
-  for line in open("maze.txt"):
-    temp = []
-    for c in line:
-      temp.append(c);
-    matrix.append(temp);
+  #prints the matrix given to it on the screen
+  def printMatrix(self):
+    for array in self.matrix:
+      for elem in array:
+        print elem,
+      print
 
-  return matrix;
+  def findE(self):
+    for index, array in enumerate(self.matrix):
+      if "E" in array:
+        coords = [index,array.index("E")]
+        return coords
+    print "couldn't find E"
 
-#prints the matrix given to it on the screen
-def printMatrix(matrix):
-  for array in matrix:
-    for elem in array:
-      print elem,
-    print
 
-def findE(matrix):
-   for index, array in enumerate(matrix):
-     if "E" in array:
-       coords = [index,array.index("E")]
+m = Matrix()
+m.printMatrix()
