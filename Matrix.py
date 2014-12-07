0a739e577ff54bf87c410cc6f82aee24da0a2cdb
Sam Kreter <sakfy6@mail.missouri.edu>
2014-12-07 13:07:18 -0600

    implemented iteritive dfs

--- Matrix.py
+++ Matrix.py
@@ -11,15 +11,8 @@ class Matrix:
         temp.append(c)
       matrix.append(temp)
     self.matrix = matrix
-    self.getStart()
-    self.graph = {}
-    self.paths = []
-    self.sPath = []
+    self.getPoints()
 
-
-
-  def printGraph(self):
-    pprint(self.graph)
 
   #prints the matrix given to it on the screen
   def printMatrix(self):
@@ -28,79 +21,37 @@ class Matrix:
         print elem,
       print
 
-  def getStart(self):
+  def getPoints(self):
     for indm, array in enumerate(self.matrix):
       for inda, elem in enumerate(array):
         if elem == "S":
-          self.startx = indm
-          self.starty = inda
-          print "found start at ",indm," ",inda
-
-  def findPath(self,x,y):
-    #should never be used but check bounds
-    if(x >= len(self.matrix) or x < 0):
-      return False
-    if(y >= len(self.matrix[x]) or y < 0):
-      return False
-
-    if(self.matrix[x][y] == "#" or self.matrix[x][y] == "+"):
-      return False
-    #check if found the end
-    if(self.matrix[x][y] == "E"):
-      return True
-
-    #mark the path
-    self.matrix[x][y] = "+"
-    if(self.findPath(x+1,y)):
-      return True
-    if(self.findPath(x,y-1)):
-      return True
-    if(self.findPath(x-1,y)):
-      return True
-    if(self.findPath(x,y+1)):
-      return True
-
-    #if all directions are face
-    self.matrix[x][y] = " "
-    return False
+          self.start = (indm,inda)
+        if elem == "E":
+          self.end = (indm,inda)
 
 
-  #creates the graph from the matrix and finds the start and end points
-  def createGraph(self):
-    for indm, array in enumerate(self.matrix):
-      for inda, elem in enumerate(array):       ############check this weird -2 thing#######
-        if inda != 0 and indm != 0 and indm < len(self.matrix)-1 and inda < len(array)-1:
-          if elem == " " or elem == "E" or elem == "S":
-            self.graph[(indm,inda)] = []
-            if self.matrix[indm-1][inda] == " " or self.matrix[indm-1][inda] == "E":
-              self.graph[(indm,inda)].append((indm-1,inda))
-            if self.matrix[indm][inda-1] == " " or self.matrix[indm][inda-1] == "E":
-              self.graph[(indm,inda)].append((indm,inda-1))
-            if self.matrix[indm+1][inda] == " " or self.matrix[indm+1][inda] == "E":
-              self.graph[(indm,inda)].append((indm+1,inda))
-            if self.matrix[indm][inda+1] == " " or self.matrix[indm][inda+1] == "E":
-              self.graph[(indm,inda)].append((indm,inda+1))
-          if elem == "E":
-            self.end = (indm,inda)
-            self.graph
-          if elem == "S":
-            self.start = (indm,inda)
-
-  #Rescusive DFS that finds all paths from start to the end
+  #dfs using a stack to find paths
   def dfs(self):
     path=[]
     stack = []
+    visited = []
     stack.append(self.start)
     while stack:
-      start = stack.pop()
-      path = path+[start]
-      if start == self.end:
-        self.paths.append(path)
-        path = []
-      else:
-        for node in self.graph[start]:
-          if node not in path:
-            stack.append(node)
+      parent = stack.pop()
+      if parent in visited:
+        continue
+      visited.append(parent)
+      children = self.getChildren(parent)
+      for child in children:
+        stack.append(child)
+
+
+  def getChildren(self,parent):
+    try:
+      if()
+    except IndexError:
+      gotdata = 'null'
+
 
 
   #from the paths found from the DFS will pick the shortest path
