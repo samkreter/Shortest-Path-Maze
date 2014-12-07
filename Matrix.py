046cf20fd9d3566bc8929c971832e6122e3d6069
Sam Kreter <sakfy6@mail.missouri.edu>
2014-12-07 14:54:34 -0600

    final commenting

--- Matrix.py
+++ Matrix.py
@@ -13,8 +13,8 @@ class Matrix:
       matrix.append(temp)
     self.matrix = matrix
     self.getPoints()
-    self.parentMap = {}
-    self.sPath = []
+    self.parentMap = {} #used to hold parents and the number of steps from the start
+    self.sPath = [] #holds the shortest path
 
 
   #prints the matrix given to it on the screen
@@ -24,6 +24,9 @@ class Matrix:
         print elem,
       print
 
+
+  #gets the start and end points from the matrix, if they don't exist
+  #if will exit the program
   def getPoints(self):
     self.start = "%"
     self.end = "%"
@@ -36,6 +39,7 @@ class Matrix:
           self.end = (indm,inda)
           print "End = ",self.end
 
+    #check if start and end exists in the matrix 
     if self.start == "%" or self.end == "%":
       print "Either Start or End was not in maze"
       sys.exit()
@@ -56,40 +60,48 @@ class Matrix:
       children = self.getChildren(parent)
       for child in children:
         stack.append(child)
+
+        #checks if there is already a parent to replace if step count is lower
         if child in self.parentMap and self.parentMap[parent][1]+1 > self.parentMap[child][1]:
           continue
         else:
           self.parentMap[child] = [parent,self.parentMap[parent][1]+1]
 
+    #check if there is no path from the start to the end
     if self.end not in self.parentMap:
       print "There is not path to the End"
       sys.exit()
+
     self.returnPath()
 
 
+  #used to get the connect children of the parent passed in
+  #used try statments to catch out of index exceptions and treats them as walls
   def getChildren(self,parent):
     indm = parent[0]
     inda = parent[1]
     children = []
-    #check child to the left of parent
+    #check child to the bottom of parent
     try:
       if self.matrix[indm-1][inda] == " " or self.matrix[indm-1][inda] == "E":
         children.append((indm-1,inda))
     except IndexError:
       pass
-
+    #checks to the right of the parent
     try:
       if self.matrix[indm][inda-1] == " " or self.matrix[indm][inda-1] == "E":
         children.append((indm,inda-1))
     except IndexError:
       pass
 
+    #checks to the top of the parent
     try:
       if self.matrix[indm+1][inda] == " " or self.matrix[indm+1][inda] == "E":
         children.append((indm+1,inda))
     except IndexError:
       pass
 
+    #checks to the left of the parent
     try:
       if self.matrix[indm][inda+1] == " " or self.matrix[indm][inda+1] == "E":
         children.append((indm,inda+1))
@@ -98,7 +110,7 @@ class Matrix:
 
     return children
 
-
+  #returns the shorts path from the end
   def returnPath(self):
     curr = self.end
     while curr != "start":
@@ -106,17 +118,6 @@ class Matrix:
       curr = self.parentMap[curr][0]
     self.showPath()
 
-
-
-
-  #from the paths found from the DFS will pick the shortest path
-  def findShortestPath(self):
-    self.sPath = self.paths[0]
-    sLength = len(self.paths[0]);
-    for path in self.paths:
-      if len(path) < sLength:
-        sLength = len(path)
-        self.sPath = path
 
   #puts the paths on the matrix to display to the user
   def showPath(self):
